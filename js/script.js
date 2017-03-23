16c0ea40d370bf3154d359f6860cc9f1009a453a
saana <saanara@hotmail.fi>
2017-03-23 08:49:56 +0200

    Fix scrolling

--- js/script.js
+++ js/script.js
@@ -9,9 +9,13 @@ $(function () {
 });
 
 var controller = new ScrollMagic.Controller();
+var css = {};
 var footer = $('.footer');
 var header = $('.header');
+var isScrolling = false;
+var lastScrollTop = $(window).scrollTop();
 var section = $('.section');
+var windowHeight = $(window).height();
 
 function pageInit() {
     mobileNavInit();
@@ -21,6 +25,9 @@ function pageInit() {
     bindSectionScrollers();
     moveHuntingHeader();
     scrollifySection();
+    $.scrollify.disable();
+    onHeaderClick();
+    scrollHandler();
 }
 
 function resizeHandler() {
@@ -31,92 +38,161 @@ function resizeHandler() {
 }
 
 function scrollifySection() {
-    if ($(window).width() > 320) {
-        $.scrollify({
-            interstitialSection : ".footer, .hungry",
-            section : ".section",
-            touchScroll: true,
-            updateHash : false
-        });
-    } else {
-        setTouchAction('auto');
-    }
+    $.scrollify({
+        section: '.section',
+        sectionName: 'section-name',
+        interstitialSection: '.footer, .wanted, .hunting',
+        scrollSpeed: 1100,
+        updateHash: true,
+        touchScroll: true,
+        overflowScroll: true
+    });
+    setTouchAction('auto');
 }
 
 function jobDescriptionToggler() {
-    $(".job__button").click(function() {
-        $(".wanted__text").find(".job-" + this.id).toggleClass("job--open");
-        if ($(window).height() < 1200) {
-            $.scrollify.update();
-        }
+    $('.job__button').click(function() {
+        $('.wanted__text').find('.job-' + this.id).toggleClass('job--open');
+        $.scrollify.enable();
+        $.scrollify.update();
+        $.scrollify.disable();
+    });
+}
+
+function onHeaderClick() {
+    $('.header__navigation a').click(function() {
+        $.scrollify.enable();
+        $.scrollify.move($(this).attr('href'));
+        $.scrollify.disable();
+        setTimeout(function() {
+            setStickyHeaderTween(false);
+        }, 1150);
     });
 }
 
 function stickyHeaderInit() {
-    var stickyHeader = new ScrollMagic.Scene({duration: 0})
-        .setPin(".header", {
-            pushFollowers: false,
-            spacerClass: "header__pin"
-        })
-        .addTo(controller);
+    var stickyHeader = new ScrollMagic.Scene({duration: 240})
+    .setPin('.header', {
+        pushFollowers: false,
+        spacerClass: 'header__pin'
+    })
+    .addTo(controller);
 
-    stickyHeader.on('end', function (event) {
-        if (event.scrollDirection === 'FORWARD') {
-            TweenMax.to(header, 0.2, {opacity: 0});
-        }
-        else {
-            TweenMax.to(header, 0.4, {opacity: 1});
-        }
-    });
-
-    setStickyHeaderTween = function(top, opacity) {
-        TweenMax.to(header, 0.2, { css: { top: top, opacity: opacity } } );
-    }
-
-    var scrollTimeout;
-    var lastScrollTop = 0;
-    var windowHeight = $(window).height();
-
-    $(window).scroll(function () {
-        if (scrollTimeout) {
-            clearTimeout(scrollTimeout);
-            scrollTimeout = null;
-        }
-        scrollTimeout = setTimeout(scrollHandler, 150);
-    });
-
-    scrollHandler = function() {
-        var scrollTop = $(this).scrollTop();
-        if ($(window).width() >= 375) {
-            handleScrollifyScrolling(lastScrollTop, scrollTop);
+    stickyHeader.on('end', function (e) {
+        if (e.scrollDirection === 'FORWARD') {
+            TweenMax.to(header, 0.4, {top: '-130px'});
         } else {
-            handleNormalScrolling(lastScrollTop, scrollTop);
+            TweenMax.to(header, 0.4, {opacity: 1, top: 0});
         }
-        lastScrollTop = scrollTop;
-    }
+    });
+}
 
-    handleNormalScrolling = function(lastScrollTop, scrollTop) {
-        stickyHeaderOnScroll(scrollTop, lastScrollTop);
+function setStickyHeaderTween(isVisible) {
+    if (!isScrolling) {
+        if (isVisible) {
+            css = {
+                opacity: 1,
+                position: 'fixed',
+                top: 0,
+                transition: 'top 0.4s'
+            };
+        } else {
+            css = {
+                position: 'fixed',
+                top: '-130px',
+                transition: 'top 0.2s'
+            }
+        }
     }
+    header.css(css);
+}
 
-    handleScrollifyScrolling = function(lastScrollTop, scrollTop) {
-        if (scrollTop === 0 || scrollTop >= lastScrollTop) {
-                if ($.scrollify.isDisabled()) { $.scrollify.enable(); }
-                setTouchAction('none');
+function scrollHandler() {
+    $(window).scroll(function() {
+        var current = $(this).scrollTop();
+        var lastSectionTop = section.last().position().top;
+        if (current + windowHeight > lastSectionTop + windowHeight) {
+            $.scrollify.disable();
+            normalScrolling(current);
+        } else if (current < lastScrollTop) {
+            handleScrollingUp(current);
+            setStickyHeaderTween(true);
+        } else if (current > lastScrollTop) {
+            handleScrollingDown(current);
+            setStickyHeaderTween(false);
         } else {
             $.scrollify.disable();
-            setTouchAction('auto');
         }
-        stickyHeaderOnScroll(scrollTop, windowHeight);
-    }
+        lastScrollTop = current;
+    });
+}
 
-    stickyHeaderOnScroll = function(scrollTop, point) {
-        if ($.scrollify.isDisabled() || (scrollTop < point)) {
-            setStickyHeaderTween(0, 1);
-        } else {
-            setStickyHeaderTween('-130px', 0);
+function normalScrolling(current) {
+    if (current > lastScrollTop) {
+        css = {
+            opacity: 1,
+            position: 'fixed',
+            top: '-130px',
+            transition: 'top 0.2s'
+        };
+    } else {
+        css = {
+            opacity: 1,
+            position: 'fixed',
+            top: 0,
+            transition: 'top 0.4s'
+        };
+    }
+    header.css(css);
+    afterScroll();
+}
+
+function handleScrollingUp(current) {
+    var sections = [];
+    section.each(function() {
+        var sectionInfo = {
+            position: $(this).position().top,
+            hash: $(this).attr('data-section-name')
+        };
+        sections.push(sectionInfo);
+    });
+
+    for (var index in sections) {
+        if (current < sections[index].position) {
+            window.location.hash = sections[index].hash;
+            break;
         }
     }
+    $.scrollify.update();
+    $.scrollify.disable();
+}
+
+function handleScrollingDown(current) {
+    if (!isScrolling) {
+        handleScrollifyTransition(current);
+    }
+}
+
+function handleScrollifyTransition(current) {
+    var overFlow = $.scrollify.current().height() - windowHeight;
+    if (current > $.scrollify.current().position().top + overFlow) {
+        isScrolling = true;
+        scrollToNext();
+    }
+}
+
+function scrollToNext() {
+    $.scrollify.enable();
+    $.scrollify.update();
+    $.scrollify.next();
+    $.scrollify.disable();
+    setTimeout(afterScroll, 1250);
+}
+
+function afterScroll() {
+    lastScrollTop = $(window).scrollTop();
+    setTouchAction('auto');
+    isScrolling = false;
 }
 
 function setTouchAction(action) {
@@ -125,7 +201,7 @@ function setTouchAction(action) {
 }
 
 function moveHuntingHeader() {
-    h1 = $('.hunting__text h1');
+    var h1 = $('.hunting__text h1');
     var img = $('.hunting__image img');
     var h1Height = img.height() / 3 * 2;
     if ($(window).width() < 992) {
@@ -137,44 +213,35 @@ function moveHuntingHeader() {
 
 function bindSectionScrollers() {
     $('.scroll__button').on('click', function() {
-        if ($(window).width() > 320 && !($.scrollify.isDisabled())) {
-            $.scrollify.next();
-        } else {
-            var id = $(this).data('href');
-            controller.scrollTo(id);
-        }
+        $.scrollify.enable();
+        $.scrollify.update();
+        $.scrollify.move($(this).attr('data-href'));
+        $.scrollify.disable();
     });
 }
 
 function scrollToId(id) {
     controller.scrollTo(id);
-
-    // if supported by the browser we can even update the URL.
     if (window.history && window.history.pushState) {
-        history.pushState("", document.title, id);
+        history.pushState('', document.title, id);
     }
 }
 
 function scrollNavInit() {
-     // change behaviour of controller to animate scroll instead of jump
-     controller.scrollTo(function (newpos) {
-         TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
-     });
+    controller.scrollTo(function (newpos) {
+        TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+    });
 
-     //  bind scroll to anchor links
-     $('a[href*=\\#]').on('click', function (e) {
-         var id = $(this).attr("href");
-         id = id.startsWith("/") ? id.substr(1) : id;
-         if ($(id).length > 0) {
-             e.preventDefault();
-             scrollToId(id);
-         }
-     });
+    $('a[href*=\\#]').on('click', function (e) {
+        var id = $(this).attr('href');
+        id = id.startsWith('/') ? id.substr(1) : id;
+        if ($(id).length > 0) {
+            e.preventDefault();
+            scrollToId(id);
+        }
+    });
 }
 
-/**
- * Mobile navigation
- */
 function mobileNavInit() {
     var mobileClass = 'header--mobile';
     if (window.matchMedia('(max-width: 767px)').matches) {
@@ -189,36 +256,33 @@ function mobileNavInit() {
         });
 
         $('.header__navigation a').click(function() {
-           showMobileNav(false);
+            showMobileNav(false);
         });
-    }
-    else {
+    } else {
         header.removeClass(mobileClass);
     }
 }
 
-function showMobileNav(open) {
+function showMobileNav(openMobileNav) {
     var openIcon = $('.header__mobile-nav-btn--open');
     var closeIcon = $('.header__mobile-nav-btn--close');
     var navList = $('.header__navigation ul');
     var navOpenClass = 'header--mobile-open';
-    if (open) {
-        console.log("open");
+    if (openMobileNav) {
         header.addClass(navOpenClass);
         openIcon.hide();
         navList.show();
         closeIcon.show();
-        header.css( 'height', '100vh' );
-        header.on('touchmove', function(event) {
-            event.preventDefault();
+        header.css('height', '100vh');
+        header.on('touchmove', function(e) {
+            e.preventDefault();
         });
     } else {
-        console.log("close");
         header.off('touchmove');
         header.removeClass(navOpenClass);
         navList.hide();
         closeIcon.hide();
         openIcon.show();
-        header.css( 'height', 'auto' );
+        header.css('height', 'auto');
     }
 }
